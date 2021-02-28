17bc6d2fadc6d316f54d6c1b932287676b9622f2
马天梦 <2541817610@qq.com>
2021-02-28 23:10:43 +0800

    :ambulance: 修复vue.config 不生效

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: mTm
  * @Date: 2021-02-20 10:34:17
- * @LastEditTime: 2021-02-25 10:37:58
+ * @LastEditTime: 2021-02-28 23:08:18
  * @LastEditors: mTm
  */
 const isProduction = process.env.NODE_ENV === 'production';
