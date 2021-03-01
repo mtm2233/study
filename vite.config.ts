f51375ab5ebae43ef4075f3ccd0842b95b1b6a52
马天梦 <2541817610@qq.com>
2021-03-01 22:28:53 +0800

    :bug: File location error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+/*
+ * @Description: 
+ * @Author: mTm
+ * @Date: 2021-03-01 22:03:18
+ * @LastEditTime: 2021-03-01 22:22:42
+ * @LastEditors: mTm
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '/@/': resolve(__dirname, 'src')
+    }
+  }
 })
