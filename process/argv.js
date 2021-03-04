c79e2abd2ff31ff3858252b4b45b2ade2d608f46
马天梦 <2541817610@qq.com>
2021-03-04 11:01:26 +0800

    :tada: init

---
+++ process/argv.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: 
+ * @Author: mTm
+ * @Date: 2021-03-04 11:00:17
+ * @LastEditTime: 2021-03-04 11:00:51
+ * @LastEditors: mTm
+ */
+process.argv.forEach(item => {
+    console.log(item);
+});
