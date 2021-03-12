0e5bf2bce736c6203914dfc9da97634ca9e23505
马天梦 <2541817610@qq.com>
2021-03-12 09:22:43 +0800

    :sparkles: sharp

--- demo/sharp/main.js
+++ demo/sharp/main.js
@@ -2,10 +2,10 @@
  * @Description: 
  * @Author: mTm
  * @Date: 2021-03-11 14:53:24
- * @LastEditTime: 2021-03-11 14:57:10
+ * @LastEditTime: 2021-03-11 17:50:19
  * @LastEditors: mTm
  */
-const fs = require('fs')
+const fs = require('fs');
 const sharp = require('sharp')
 
 sharp('../../public/img/pkq.jpg').resize(100,100).rotate(45).toBuffer().then(data => {
