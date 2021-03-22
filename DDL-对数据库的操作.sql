3fec5b6aa096f2f110db3a1d1b61ee31ee6b327e
马天梦 <2541817610@qq.com>
2021-03-22 22:20:21 +0800

    :sparkles: 添加外键

--- DDL-对数据库的操作.sql
+++ DDL-对数据库的操作.sql
@@ -2,23 +2,23 @@
 --  可以通过DDL语句对数据库或者表进行：创建、删除、修改等操作；
 
 # 查看所有的数据库
--- SHOW DATABASES;
+SHOW DATABASES;
 
 # 选择某一个数据库
--- USE study;
+USE study;
 
 # 查看当前正在使用的数据库
--- SELECT DATABASE();
+SELECT DATABASE();
 
 # 创建一个新的数据库
--- CREATE DATABASE isma;
--- CREATE DATABASE IF NOT EXISTS isma;
--- CREATE DATABASE IF NOT EXISTS isma
--- DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+CREATE DATABASE isma;
+CREATE DATABASE IF NOT EXISTS isma;
+CREATE DATABASE IF NOT EXISTS isma
+DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 # 删除数据库
--- DROP DATABASE isma;
--- DROP DATABASE IF EXISTS isma;
+DROP DATABASE isma;
+DROP DATABASE IF EXISTS isma;
 
 # 修改数据库的编码
 ALTER DATABASE isma CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;
--- DDL-对数据表的操作.sql
+++ DDL-对数据表的操作.sql
@@ -2,24 +2,24 @@
 --  可以通过DDL语句对数据库或者表进行：创建、删除、修改等操作；
 
 # 查看所有的表
--- SHOW TABLES;
+SHOW TABLES;
 
 # 新建表
--- CREATE TABLE isma;
--- CREATE TABLE IF NOT EXISTS isma(
--- 	name VARCHAR(20),
--- 	age INT
--- );
+CREATE TABLE isma;
+CREATE TABLE IF NOT EXISTS isma(
+	name VARCHAR(20),
+	age INT
+);
 
 # 删除表
--- DROP TABLE isma;
--- DROP TABLE IF EXISTS isma;
+DROP TABLE isma;
+DROP TABLE IF EXISTS isma;
 
 # 查看表的结构
--- DESC isma;
+DESC isma;
 
 # 查看创建表的SQL语句
--- SHOW CREATE TABLE ismaa;
+SHOW CREATE TABLE ismaa;
 
 # 完整的创建表的语法
 CREATE TABLE IF NOT EXISTS user (
@@ -32,24 +32,24 @@ CREATE TABLE IF NOT EXISTS user (
 
 # 修改表
 # 1.修改表的名字
--- ALTER TABLE isma RENAME TO ismaa;
+ALTER TABLE isma RENAME TO ismaa;
 # 2.添加一个新的列
--- ALTER TABLE ismaa ADD `publishTime` DATETIME;
--- ALTER TABLE ismaa ADD `updateTime` DATETIME;
+ALTER TABLE ismaa ADD `publishTime` DATETIME;
+ALTER TABLE ismaa ADD `updateTime` DATETIME;
 # 3.修改字段的名称
--- ALTER TABLE ismaa CHANGE `publishTime` `publishTime1` DATE;
+ALTER TABLE ismaa CHANGE `publishTime` `publishTime1` DATE;
 # 4.修改字段的类型
--- ALTER TABLE ismaa MODIFY publishTime DATETIME;
+ALTER TABLE ismaa MODIFY publishTime DATETIME;
 # 5.删除某一个字段
--- ALTER TABLE ismaa DROP `updateTime`;
+ALTER TABLE ismaa DROP `updateTime`;
 
 # 补充
 # 根据一个表结构去创建另外一张表
--- CREATE TABLE ismaaaa LIKE user;
+CREATE TABLE ismaaaa LIKE user;
 
 # 根据另外一个表中的所有内容，创建一个新的表
 
--- CREATE TABLE user2 (SELECT * FROM products);
+CREATE TABLE user2 (SELECT * FROM products);
 
 
 
