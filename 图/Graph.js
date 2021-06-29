605641ecee4dffa54d4cb3d9e18f943d6033f086
MA <2541817610@qq.com>
2021-06-29 14:11:23 +0800

    feat: Graph dfs

--- 图/Graph.js
+++ 图/Graph.js
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: mTm
  * @Date: 2021-06-28 15:59:10
- * @LastEditTime: 2021-06-29 10:50:51
+ * @LastEditTime: 2021-06-29 13:37:32
  * @LastEditors: mTm
  */
 // 节点
@@ -81,43 +81,74 @@ class Graph {
     // prev用来记录搜索路径
     let prev = new Array(this.v).fill(-1);
     // 访问s
-    queue.push(s)
+    queue.push(s);
     visited[s] = 1;
     while (queue.length) {
       const i = queue.shift();
       // 避免重复访问
       const vertexs = this.obj[i].toString().filter((i) => !visited[i]);
       // 记录访问状态，访问顶点
-      vertexs.forEach(vertex => {
-        prev[vertex] = i
-        visited[vertex] = 1
-      })
+      vertexs.forEach((vertex) => {
+        prev[vertex] = i;
+        visited[vertex] = 1;
+      });
       // 加入队列
       queue.push(...vertexs);
       if (i === t) {
         // 输出路径
-        return this.printf(prev, s, t)
+        return this.printf(prev, s, t);
       }
     }
-    return -1
+    return -1;
   }
   printf(prev, s, t) {
-    const path = []
+    const path = [];
     const printf_c = (prev, s, t) => {
       if (prev[t] !== -1 && t !== s) {
-        printf_c(prev, s, prev[t])
+        printf_c(prev, s, prev[t]);
       }
-      path.push(t)
-    }
-    printf_c(prev, s, t)
-    return path
+      path.push(t);
+    };
+    printf_c(prev, s, t);
+    return path;
+  }
+
+  // 深度优先搜索
+  // 走迷宫
+  dfs(s, t) {
+    // 全局变量或者类成员变量
+    let found = false;
+    // visited是用来记录已经被访问的顶点，用来避免顶点被重复访问。
+    let visited = new Array(this.v).fill(0);
+    // prev用来记录搜索路径
+    let prev = new Array(this.v).fill(-1);
+    const recurDfs = (s, t, visited, prev) => {
+      if (found) {
+        return;
+      }
+      visited[s] = 1;
+      if (s === t) {
+        found = true;
+        return;
+      }
+      this.obj[s].toString().forEach(q => {
+        if (!visited[q]) {
+          prev[q] = s
+          recurDfs(q, t, visited, prev)
+        }
+      })
+    };
+    recurDfs(s, t, visited, prev);
+    return this.printf(prev, s, t);
   }
 }
 
 const graph = new Graph(10);
 graph.addEdge(0, 1);
+graph.addEdge(0, 9);
 graph.addEdge(0, 5);
 graph.addEdge(1, 6);
 graph.addEdge(6, 9);
-console.log(graph.bfs(0, 9));
+console.log('bfs', graph.bfs(0, 9));
+console.log('dfs', graph.dfs(0, 9));
 // console.log(graph.obj);
