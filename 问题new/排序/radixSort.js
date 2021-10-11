f48a9ace5d10f8ce1a8a85e29db0ccce08fb784f
MA <2541817610@qq.com>
2021-10-11 23:16:26 +0800

    feat: sort/radixSortLSD

--- 问题new/排序/radixSort.js
+++ 问题new/排序/radixSort.js
@@ -2,8 +2,65 @@
  * @Description: 基数排序
  * @Author: mTm
  * @Date: 2021-10-11 09:03:05
- * @LastEditTime: 2021-10-11 09:03:07
+ * @LastEditTime: 2021-10-11 23:15:27
  * @LastEditors: mTm
  */
 // MSD：先从高位开始进行排序，在每个关键字上，可采用计数排序
 // LSD：先从低位开始进行排序，在每个关键字上，可采用桶排序
+
+function radixSortMSD(params) {
+  
+}
+
+// 计数排序
+function countSort(arr, index) {
+  // 计数
+  const count = new Array(10).fill(0)
+  const len = arr.length
+  for (let i = 0; i < len; i++) {
+    count[arr[i][index]]++
+  }
+
+  // 累加
+  for (let i = 1; i < 10; i++) {
+    count[i] += count[i - 1]
+  }
+
+  // 排序
+  const handlerArr = new Array(len)
+  for (let i = len - 1; i >= 0; i--) {
+    handlerArr[--count[arr[i][index]]] = arr[i]
+  }
+
+  return  handlerArr
+}
+
+function radixSortLSD(arr) {
+  const len = arr.length;
+  let arrCopy = arr.map(String)
+
+  // 最长的长度
+  let maxLength = arrCopy[0].length;
+  for (let i = 1; i < len; i++) {
+    const l = arrCopy[i].length
+    if (maxLength < l) {
+      maxLength = l
+    }
+  }
+
+  // 补0
+  arrCopy = arrCopy.map(v => v.padStart(maxLength, '0'));
+
+  while (maxLength) {
+    arrCopy = countSort(arrCopy, --maxLength)
+  }
+  
+  for (let i = 0; i < len; i++) {
+    arr[i] = Number(arrCopy[i])
+  }
+}
+
+module.exports = {
+  radixSortMSD,
+  radixSortLSD,
+}
--- 问题new/排序/test.js
+++ 问题new/排序/test.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: mTm
  * @Date: 2021-10-06 21:08:44
- * @LastEditTime: 2021-10-11 22:37:43
+ * @LastEditTime: 2021-10-11 23:03:41
  * @LastEditors: mTm
  */
 const bubbleSort = require('./bubbleSort')
@@ -12,6 +12,7 @@ const mergeSort = require('./mergeSort')
 const quickSort = require('./quickSort')
 const bucketSort = require('./bucketSort')
 const countSort = require('./countSort')
+const {radixSortMSD, radixSortLSD} = require('./radixSort')
 
 // 生成大小为start~end、长度为nums的数组
 function getArr(start, end, nums) {
@@ -23,8 +24,8 @@ function getArr(start, end, nums) {
   return randomArr
 }
 // const arr = getArr(-10, 15, 20);
-const arr = getArr(-100000, 10000000, 50000000);
-// const arr = getArr(0, 10, 10);
+// const arr = getArr(-100000, 10000000, 50000000);
+const arr = getArr(100000000, 9999999999, 10);
 const start = new Date().getTime(); // 开始时间
 
 // bubbleSort(arr)
@@ -32,8 +33,9 @@ const start = new Date().getTime(); // 开始时间
 // selectionSort(arr)
 // mergeSort(arr)
 // quickSort(arr)
-bucketSort(arr)
+// bucketSort(arr)
 // countSort(arr)
+radixSortLSD(arr)
 
 const end = new Date().getTime(); // 结束时间
 console.log(arr);
