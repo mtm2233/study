ab24cbcaf0ad2d8ec73d675fc41b6db6a3f267ba
MA <2541817610@qq.com>
2021-11-17 12:52:00 +0800

    feat: graph(bfs、dfs)

--- 数据结构New/图/AdjacencyList.js
+++ 数据结构New/图/AdjacencyList.js
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: mTm
  * @Date: 2021-11-14 18:17:29
- * @LastEditTime: 2021-11-14 22:30:24
+ * @LastEditTime: 2021-11-17 11:53:38
  * @LastEditors: mTm
  */
 const SkipList = require('../跳表/SkipList');
@@ -49,7 +49,7 @@ class AdjacencyList {
 
   isEdge(vertex1, vertex2) {
     this.init(vertex1, vertex2);
-    const val = this.vertexs[vertex1].binarySearch(vertex2)
+    const val = this.vertexs[vertex1].binarySearch(vertex2);
     return val !== -1 ? val.weight : false;
   }
 
@@ -59,13 +59,93 @@ class AdjacencyList {
     return Object.keys(this.vertexs)
       .map(ver => this.vertexs[ver].binarySearch(vertex))
       .filter(v => v !== -1)
-      .map(v => v.weight === true ? v.vertex : v);
+      .map(v => (v.weight === true ? v.vertex : v));
   }
 
   // 出度
   // 表⽰有多少条边是以这个顶点为起点指向其他顶点
   outDegree(vertex) {
-    return this.vertexs[vertex].showAll().map(v => v.weight === true ? v.vertex : v);
+    return this.vertexs[vertex]
+      .showAll()
+      .map(v => (v.weight === true ? v.vertex : v));
+  }
+
+  printPath(prev, s, t) {
+    const path = [t];
+    while (prev[t] && t !== s) {
+      path.unshift(prev[t]);
+      t = prev[t];
+    }
+    return path;
+  }
+
+  // 广度优先搜索
+  // ⽆权图来说，找到的就是最短路径
+  bfs(s, t) {
+    if (s === t) {
+      return [s];
+    }
+    // 存储已经被访问、但相连的顶点还没有被访问的顶点
+    const queue = [];
+    // 记录已经被访问的顶点，⽤来避免顶点被重复访问
+    const visited = [];
+    // 搜索路径
+    const prev = [];
+    queue.push(s);
+    visited[s] = true;
+    prev[s] = -1;
+
+    while (queue.length) {
+      const v = queue.shift();
+      const vData = this.vertexs[v].showAll();
+      for (let i = 0; i < vData.length; i++) {
+        const { vertex } = vData[i];
+        if (!visited[vertex]) {
+          prev[vertex] = v;
+          if (vertex === t) {
+            return this.printPath(prev, s, t);
+          }
+          visited[vertex] = true;
+          queue.push(vertex);
+        }
+      }
+    }
+  }
+
+  // 深度优先搜索
+  dfs(s, t) {
+    const visited = [];
+    const prev = [];
+    // 是否找到t
+    const found = {
+      value: false,
+    };
+    prev[s] = -1;
+
+    this.dfs_c(s, t, visited, prev, found);
+    if (found.value) {
+      return this.printPath(prev, s, t);
+    }
+  }
+
+  dfs_c(w, t, visited, prev, found) {
+    if (found.value) {
+      return;
+    }
+    if (w === t) {
+      found.value = true;
+      return;
+    }
+    visited[w] = true;
+
+    const wData = this.vertexs[w].showAll();
+    for (let i = 0; i < wData.length; i++) {
+      const { vertex } = wData[i];
+      if (!visited[vertex]) {
+        prev[vertex] = w;
+        this.dfs_c(vertex, t, visited, prev, found);
+      }
+    }
   }
 }
 
--- 数据结构New/图/test.js
+++ 数据结构New/图/test.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: mTm
  * @Date: 2021-11-14 16:51:55
- * @LastEditTime: 2021-11-14 22:31:36
+ * @LastEditTime: 2021-11-17 11:28:40
  * @LastEditors: mTm
  */
 const AdjacencyMatrix = require('./AdjacencyMatrix')
@@ -11,20 +11,25 @@ const adjacencyMatrix = new AdjacencyMatrix({
   directed: true
 })
 
-adjacencyMatrix.add(1, 2, 3)
-adjacencyMatrix.add(2, 5, 5)
-adjacencyMatrix.add(5, 1, 0)
-// adjacencyMatrix.remove(5, 1)
+// adjacencyMatrix.add(1, 2, 3)
+// adjacencyMatrix.add(2, 5, 5)
+// adjacencyMatrix.add(5, 1, 0)
+// // adjacencyMatrix.remove(5, 1)
 
-console.log(adjacencyMatrix.inDegree(1));
-console.log(adjacencyMatrix.outDegree(1));
+// console.log(adjacencyMatrix.inDegree(1));
+// console.log(adjacencyMatrix.outDegree(1));
 
-// const AdjacencyList = require('./AdjacencyList')
-// const adjacencyList = new AdjacencyList({
-//   directed: true
-// })
-// adjacencyList.add(1, 2, 8)
-// adjacencyList.add(2, 5)
-// adjacencyList.add(5, 1)
+const AdjacencyList = require('./AdjacencyList')
+const adjacencyList = new AdjacencyList({
+  // directed: true
+})
+adjacencyList.add(1, 2)
+adjacencyList.add(1, 6)
+adjacencyList.add(2, 5)
+adjacencyList.add(5, 1)
 // console.log(adjacencyList.inDegree(9));
 // console.log(adjacencyList.outDegree(1));
+const bfsPath = adjacencyList.bfs(1, 5) || []
+const dfsPath = adjacencyList.dfs(1, 5) || []
+console.log('bfs',bfsPath.join('->'));
+console.log('dfs',dfsPath.join('->'));
