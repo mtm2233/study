cd4f8d91c303dba2a8de4d946b56b2ddbb6998f3
MA <2541817610@qq.com>
2022-02-20 23:10:16 +0800

    feat: create test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 /*
- * @Description: 
+ * @Description:
  * @Author: mTm
  * @Date: 2021-07-15 11:11:21
- * @LastEditTime: 2021-12-23 17:00:49
+ * @LastEditTime: 2022-02-07 09:12:51
  * @LastEditors: mTm
  */
 // const obj = {
@@ -65,7 +65,7 @@
 // // for (const key in object) {
 // //     if (Object.hasOwnProperty.call(object, key)) {
 // //         const element = object[key];
-        
+
 // //     }
 // // }
 
@@ -79,10 +79,102 @@
 // a1.next()
 // a1.next()
 
-
 // let text = '****e123456bcd--c--'
 
 // text = '****e123456bcd--c--'.split("")
 // console.log(text);
 // text.splice(11, 12)
 // console.log(text);
+
+// console.log(aaa);
+
+// var aaa = 5
+
+// function a(b) {
+//   console.log(b);
+// }
+
+// a()
+
+// const obj1 = {
+//   website: "http://"
+// }
+
+// const age = 18
+// const obj = {
+//   eating() {
+//     console.log(age);
+//   }
+// }
+
+// obj.eating()
+
+// const obj = {
+//   name: 'obj',
+//   eating() {
+//     setTimeout(() => {
+//       console.log(this);
+//     }, 100);
+//   }
+// }
+
+// obj.eating.call('abc')
+
+// function Person(name) {
+//   this.name = name;
+//   console.log(this);
+//   return {
+//     name: 'preson'
+//   }
+// }
+
+// const person1 = new Person('person1')
+// console.log(person1);
+
+// Function.prototype.mcall = function (thisArg) {
+//   thisArg.fn = this
+//   // console.log(this, this.this);
+//   thisArg.fn()
+// }
+
+// function foo(params) {
+//   console.log('foo', this);
+// }
+
+// foo.mcall({name: 'bar'})
+
+// const numObj = new Number(5);
+// numObj.fn = function(name = 'fn', a, b, c) {
+//   console.log(name, arguments);
+// };
+// numObj.fn(undefined, 'a', 'b', 'c');
+
+// function foo(name) {
+//   console.log(name);
+// }
+
+// foo.apply({}, ['a'])
+
+// const age = 'windows';
+// function foo() {
+//   const age = 'foo';
+//   console.log(arguments);
+// }
+
+// foo.bind(123, 'a')('c', 'd')
+
+// function Bar() {
+//   var fNOP = function() {}
+//   console.log(this, fNOP, this instanceof fNOP);
+// }
+
+// new Bar()
+
+
+
+// function foo(params) {
+//   var a = b = 100
+// }
+// foo()
+// // console.log(a);
+// console.log(b);
